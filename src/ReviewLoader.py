0ef1752b0b45f6684f02c5def8fe9d86b187dc57
maccuryj <jean.maccury@gmail.com>
2020-04-02 17:26:55 +0200

    Review Dataset and Dataloader resources

--- src/ReviewLoader.py
+++ src/ReviewLoader.py
@@ -1,53 +1,106 @@
 import os
 import csv
 import numpy
+import pickle
+import torch
 import torch.nn as nn
+from torch.nn.utils.rnn import pad_sequence, pack_padded_sequence
+from torch.nn.functional import one_hot
 from torch.utils.data import DataLoader, IterableDataset
+from sklearn.feature_extraction.text import TfidfVectorizer
 
-class ProductReviews():    
+class ProductReviews():
+    """
+    Product Review Data Class that provides helper function for building
+    necessary processing resources.
+    Provides review dataloader for easy integration with PyTorch.
 
-    def __init__(self, dir):
+    Attributes:
+        root_dir (str):                 absolute path of review directory
+        word2id (dict):                 word to index dictionary
+    """    
+
+    def __init__(self, review_dir):        
+        self.review_dir = review_dir              
+        self.word2id = {}
+        self.id2word = {}
+
+    def create_vocabulary(self, file, max_features=10000):
         """
-        Dataset creation class, providing on instantiation a stratified train/test split.
-        Provides review dataloader for easy integration with PyTorch.
-
-        Attributes:
-            root_dir (str):                 absolute path of review directory
+        Make use of Term Frequency - Inverse Document Frequency to create a vocabulary
+        for use in the RNN. The encoding is adjusted for further needs and a decoding
+        dictionary is created.
         """
-        self.review_dir = review_dir
+        vectorizer = TfidfVectorizer(max_features=max_features)
+        with open(os.path.join(self.review_dir, file), 'r') as f:
+            words = vectorizer.fit_transform(f)
+        self.word2id = vectorizer.vocabulary_
+        
+        self.adjust_encoding()
+        self.create_decoding()        
 
-        reviews = get_reviews()
+        return vectorizer, words
 
+    def adjust_encoding(self):
+        if 0 in self.word2id.values():
+            for word in self.word2id:
+                if self.word2id[word] == 0:
+                    self.word2id[word] == len(self.word2id)
 
-    def get_reviews(self, size):
-        corpus = []
-        product2idx = {}        
-        for i, file in enumerate(os.listdir(self.dir)):
-            if '.csv' in file:
-                product2idx[i] = file[:-4]
-                with open(os.path.join(data_folder, file), newline='') as f:
-                    reader = csv.reader(f)
-                    for i, row in enumerate(reader):
-                        while len(corpus) < size:
-                            corpus.append(row[0])
+        encoding_size = len(self.word2id)
+        if "UNK" not in self.word2id.values():
+            self.word2id["UNK"] = encoding_size
+
+    def create_decoding(self):
+        for word in self.word2id:
+            self.id2word[self.word2id[word]] = word
+
+    def save_vocab(tfidf, words, filename):
+        pickle.dump(tfidf, open(os.path.join(review_dir, "tfidf.pickle")))
+        pickle.dump(words, open(os.path.join(review_dir, "wordlist.pickle")))
+
+    def load_vocab():
+        pass
+
 
 class ReviewDataset(IterableDataset):
 
-    def __init__(self, file):
+    def __init__(self, file, encoding):
         self.file = file
+        self.encoding = encoding
 
     def parse_file(self):
         with open(self.file, 'r') as review_file:
             reader = csv.reader(review_file)
-            for line in reader:
-                yield line
+            for line in reader:             
+                yield from line
+
+    def __iter__(self):
+        return self.parse_file()
+
+class Collator():
+    
+    def __init__(self, encoding):
+        self.encoding = encoding    
+
+    def __call__(self, batch):
+        encoded_batch = []
+        batch_lengths = []
+
+        for line in batch:
+            encoded_line = [self.encoding[word] if word in self.encoding.keys()
+            else self.encoding["UNK"]
+            for word in line.split(' ')]
+            #Do we need One-Hot?
+            encoded_batch.append(one_hot(torch.LongTensor(encoded_line), num_classes=len(self.encoding)-1))
+            batch_lengths.append(len(encoded_line))
+        
+        batch_tensor = pad_sequence(encoded_batch, batch_first=True, padding_value=0)
+        padded_batch = pack_padded_sequence(batch_tensor, batch_lengths, batch_first=True, enforce_sorted=False)
+        return padded_batch, batch_lengths
+        
 
 
-if __name__ == "__main__":
 
-    dataset = ReviewDataset("reviews.csv")
-    loader = DataLoader(dataset, batch_size=3)
 
-    for inp in loader:
-        print(inp)
     
