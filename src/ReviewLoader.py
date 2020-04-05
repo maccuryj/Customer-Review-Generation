7ff69beb2f0162eabdeb2030c9b954b1598af871
maccuryj <jean.maccury@gmail.com>
2020-04-05 12:56:25 +0200

    added functionality

--- src/ReviewLoader.py
+++ src/ReviewLoader.py
@@ -2,6 +2,8 @@ import os
 import csv
 import numpy
 import pickle
+import unicodedata
+from itertools import chain
 import torch
 import torch.nn as nn
 from torch.nn.utils.rnn import pad_sequence, pack_padded_sequence
@@ -24,14 +26,18 @@ class ProductReviews():
         self.review_dir = review_dir              
         self.word2id = {}
         self.id2word = {}
+        self.encoding_filename = ""
 
-    def create_vocabulary(self, file, max_features=10000):
+    def create_vocabulary(self, file, max_features=10000, tfidf=False):
         """
-        Make use of Term Frequency - Inverse Document Frequency to create a vocabulary
-        for use in the RNN. The encoding is adjusted for further needs and a decoding
+        Make use of Term Frequency and (if set) Inverse Document Frequency to create a vocabulary
+        for use in the RNN. The encoding is then adjusted and a decoding
         dictionary is created.
         """
-        vectorizer = TfidfVectorizer(max_features=max_features)
+        if tfidf:
+            vectorizer = TfidfVectorizer(max_features=max_features,  token_pattern=r"(?u)\b\w+\b")
+        else:
+            vectorizer = TfidfVectorizer(max_features=max_features, use_idf=False,  token_pattern=r"(?u)\b\w+\b")
         with open(os.path.join(self.review_dir, file), 'r') as f:
             words = vectorizer.fit_transform(f)
         self.word2id = vectorizer.vocabulary_
@@ -42,10 +48,14 @@ class ProductReviews():
         return vectorizer, words
 
     def adjust_encoding(self):
+        """
+        Adjust the word encoding to include language tokens
+        """
         if 0 in self.word2id.values():
             for word in self.word2id:
                 if self.word2id[word] == 0:
                     self.word2id[word] == len(self.word2id)
+        self.word2id["<PAD>"] = 0
 
         encoding_size = len(self.word2id)
 
@@ -57,15 +67,51 @@ class ProductReviews():
                 i += 1
 
     def create_decoding(self):
+        """
+        Create the word decoding dictionary from the encoding dictionary
+        """
         for word in self.word2id:
             self.id2word[self.word2id[word]] = word
 
-    def save_vocab(tfidf, words, filename):
-        pickle.dump(tfidf, open(os.path.join(review_dir, "tfidf.pickle")))
-        pickle.dump(words, open(os.path.join(review_dir, "wordlist.pickle")))
+    def get_reviewloader(self, batch_size):
+        """
+        Creates PyTorch Dataloader for reviews
 
-    def load_vocab():
-        pass
+        Args:
+            batch_size (int):               Batch size for DataLoader
+        """
+        collator = Collator(self.word2id)
+        loader = DataLoader(dataset, batch_size=batch_size, collate_fn=collator)
+
+        return loader
+
+    def save_vocab(filename):
+        """
+        Save a vocabulary on disk.
+
+        Args:
+            filename (str):                 Name of file to save the words
+        """
+        if '.pickle' not in filename:
+                filename = filename + '.pickle'
+
+        pickle.dump(word2id, open(os.path.join(review_dir, filename)))
+        self.encoding_filename = filename
+
+
+    def load_vocab(filename):
+        """
+        Load a vocabulary from disk.
+
+        Args:
+            filename (str):                 Name of file containing the words
+        """        
+        if '.pickle' not in filename:
+                filename = filename + '.pickle'
+        word2id = pickle.load(open(os.path.join(review_dir, filename)))
+        self.create_decoding()
+
+        return word2id
 
 
 class ReviewDataset(IterableDataset):
@@ -80,8 +126,12 @@ class ReviewDataset(IterableDataset):
             for line in reader:             
                 yield from line
 
+    def get_stream():
+
+
     def __iter__(self):
         return self.parse_file()
+
 
 class Collator():
     
@@ -96,24 +146,26 @@ class Collator():
         Y_len = []
 
         for line in batch:
-            encoded_line = [self.encoding[word] if word in self.encoding.keys()
+            encoded_line = [self.encoding[word.lower()] if word.lower() in self.encoding.keys()
             else self.encoding["<UNK>"]
             for word in line.split(' ')]
             encoded_line.insert(0, self.encoding["<SOR>"])
             encoded_line.append(self.encoding["<EOR>"])
-
             
-            X.append(one_hot(torch.LongTensor(encoded_line[:-1])))            
+            X.append(one_hot(torch.LongTensor(encoded_line[:-1])).float())         
             X_len.append(len(encoded_line[:-1]))
-            Y.append(one_hot(torch.LongTensor(encoded_line[1:])))            
+            Y.append(torch.LongTensor(encoded_line[1:]))            
             Y_len.append(len(encoded_line[1:]))
         
         X_padded = pad_sequence(X, batch_first=True, padding_value=0)
         Y_padded = pad_sequence(Y, batch_first=True, padding_value=0)  
         X_packed = pack_padded_sequence(X_padded, X_len, batch_first=True, enforce_sorted=False)
-        Y_packed = pack_padded_sequence(Y_padded, Y_len, batch_first=True, enforce_sorted=False)
-        return X_packed, X_len, Y_packed, Y_len
         
+        return X_packed, X_len, Y_padded, Y_len
+        
+
+
+
 
 
 
